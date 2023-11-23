caed6adae8d7c91d190c22520ea677ca31126581
dazen1 <danzeevi1@gmail.com>
2023-11-23 17:15:06 +0200

    *fixed button classes

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,11 +1,23 @@
+import { Link } from "react-router-dom";
 
-export function Button({icon ,classname ='', text}) {
-  return (
-    <button className={`button ${classname}`}>
-      <div className="icon-holder">
-        {icon}
-      </div>
-        {text}
-    </button>
-  )
-}
+// export function Button({icon ,classname ='', text ,linkTo = '/'}) {
+//   return (
+//     <Link to={linkTo} className={`button ${classname}`}>
+//       <div className="icon-holder">
+//         {icon}
+//       </div>
+//         {text}
+//     </Link>
+//   )
+// }
+export function Button({icon ,classname ='', text }) {
+    return (
+      <button  className={`action-button ${classname}`}>
+        <div className="icon-holder">
+          {icon}
+        </div>
+          {text}
+      </button>
+    )
+  }
+  
--- src/views/StayDetails.jsx
+++ src/views/StayDetails.jsx
@@ -7,11 +7,16 @@ import { MdIosShare as ShareIcon } from "react-icons/md";
 
 import { Button } from "../components/UI/Button";
 
-
 // for now
-export const imgUrls = ["https://a0.muscache.com/im/pictures/dc89cf8d-6d45-4db8-acb0-e8331150d725.jpg?im_w=960","https://a0.muscache.com/im/pictures/3ecf9b60-50a1-4a8b-9aa5-7f7c9bd2defa.jpg?im_w=480","https://a0.muscache.com/im/pictures/e362a6b6-ef1e-43c5-9186-ac26abf2994e.jpg?im_w=480","https://a0.muscache.com/im/pictures/c566639d-6444-458e-83e1-2ec903f59e36.jpg?im_w=480","https://a0.muscache.com/im/pictures/miso/Hosting-48086771/original/45f8627a-db65-426e-aab5-8018b4b40e17.jpeg?im_w=1200"]
+export const imgUrls = [
+  "https://a0.muscache.com/im/pictures/dc89cf8d-6d45-4db8-acb0-e8331150d725.jpg?im_w=960",
+  "https://a0.muscache.com/im/pictures/3ecf9b60-50a1-4a8b-9aa5-7f7c9bd2defa.jpg?im_w=480",
+  "https://a0.muscache.com/im/pictures/e362a6b6-ef1e-43c5-9186-ac26abf2994e.jpg?im_w=480",
+  "https://a0.muscache.com/im/pictures/c566639d-6444-458e-83e1-2ec903f59e36.jpg?im_w=480",
+  "https://a0.muscache.com/im/pictures/miso/Hosting-48086771/original/45f8627a-db65-426e-aab5-8018b4b40e17.jpeg?im_w=1200",
+];
 
-// 
+//
 export function StayDetails() {
   // TODO: get the stay from the store
   const { stayId } = useOutletContext();
@@ -22,16 +27,15 @@ export function StayDetails() {
       <Link to="/">←back</Link>
       <div className="details-header">
         <h3>stay name {stayId}</h3>
-        <div className="action-btn"><Button icon={<Heart />} text={"save"} /></div>
-        <div className="action-btn"><Button icon={<ShareIcon />} text={"share"} /></div>
+          <Button icon={<Heart />} text={"save"} />
+          <Button icon={<ShareIcon />} text={"share"} />
       </div>
-    
+
       <ImageShortGalery imgUrls={imgUrls} />
       <div className="checkout-container">
-        <StayDescription/>
-        <StayCheckout/>
+        <StayDescription />
+        <StayCheckout />
       </div>
-      
     </>
   );
 }
