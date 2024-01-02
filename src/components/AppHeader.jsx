3edd7aafecd235827afd7ec5ba35a90da328650f
YardenheStudent <yardenhe@post.bgu.ac,il>
2024-01-02 21:44:37 +0200

    *.Style- mode style to app header

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -11,7 +11,6 @@ export function AppHeader() {
     const [isOpenFilter, onToggle] = useToggle()
     const isFirstRender = useRef(true);
 
-
     useEffect(() => {
         const handleScroll = () => {
             if (isOpenEffect) {
@@ -51,73 +50,67 @@ export function AppHeader() {
         <>
             {isOpenFilter && <div className="overlay" onClick={onToggleEffect}></div>}
 
-            {!isOpenFilter && <header className='app-header'>
 
-                <Link to="/">
+            <header className={!isOpenFilter ? 'app-header' : ' app-header-filter'}>
+
+                {!isOpenFilter && <Link to="/">
                     <img className="app-header-logo" src="img/airbnb-logoo.PNG" />
-                </Link>
+                </Link>}
 
-                <section className={`date-picker${isOpenEffect ? ' enlarge' : ' '}`} onClick={onToggleEffect}>
+                {isOpenFilter &&
+                    <section className="app-header grid-app-header">
+                        <Link to="/">
+                            <img className="app-header-logo" src="img/airbnb-logoo.PNG" />
+                        </Link>
+                        <section className='app-mini-menu'>
+                            <section >Stays</section>
+                            <section >Experience</section>
+                            <section >Online Experience </section>
+                        </section>
+
+
+                        <section className='right-header-menu'>
+                            <div className='switchlen-menu'>
+                                <button>Switch to hosting</button>
+                                <LuGlobe className='global-btn' />
+                            </div>
+                            <div className='menu-bar'> <IoMdMenu className='menu-icon' /><div className='circle'>י</div></div>
+
+                        </section>
+                    </section>}
+                {!isOpenFilter ? <section className={`date-picker${isOpenEffect ? ' enlarge' : ' '}`} onClick={onToggleEffect}>
                     <section className='btn-datepicker bold'>AnyWhere</section>
                     <section className='btn-datepicker bold'>Any Week</section>
                     <section className='btn-datepicker'>Add guests  </section>
                     <IoSearch className='search-btn' />
-                </section>
-                <section className='right-header-menu'>
+                </section> :
+                    <section className={`date-picker grid-date-picker${!isOpenEffect ? ' shrink' : ' '}`} onClick={onToggleEffect}>
+                        <section className='btn-datepicker'>
+                            <span className='bold'>Where</span>
+                            <p >Search destinations</p>
+                        </section>
+                        <section className='btn-datepicker'>
+                            <span className='bold'>Check in</span>
+                            <p >Add dates</p>
+                        </section>
+                        <section className='btn-datepicker'>
+                            <span className='bold'>Check out</span>
+                            <p >Add dates</p>
+                        </section>
+                        <section className='btn-datepicker'>
+                            <span className='bold'>Who</span>
+                            <p className='block' >Add guests</p>
+                        </section>
+                        <IoSearch className='search-btn' />
+                    </section>}
+                {!isOpenFilter && <section className='right-header-menu'>
                     <div className='switchlen-menu'>
                         <button>Switch to hosting</button>
                         <LuGlobe className='global-btn' />
                     </div>
                     <div className='menu-bar'> <IoMdMenu className='menu-icon' /><div className='circle'>י</div></div>
-
-                </section>
-
-
-            </header>}
-            {isOpenFilter && <header className='app-header-filter'>
-
-
-                <section className="app-header grid-app-header">
-                    <Link to="/">
-                        <img className="app-header-logo" src="img/airbnb-logoo.PNG" />
-                    </Link>
-                    <section className='app-mini-menu'>
-                        <section >Stays</section>
-                        <section >Experience</section>
-                        <section >Online Experience </section>
-                    </section>
-
-
-                    <section className='right-header-menu'>
-                        <div className='switchlen-menu'>
-                            <button>Switch to hosting</button>
-                            <LuGlobe className='global-btn' />
-                        </div>
-                        <div className='menu-bar'> <IoMdMenu className='menu-icon' /><div className='circle'>י</div></div>
-
-                    </section>
-                </section>
-                <section className={`date-picker grid-date-picker${!isOpenEffect ? ' shrink' : ' '}`} onClick={onToggleEffect}>
-                    <section className='btn-datepicker'>
-                        <span className='bold'>Where</span>
-                        <p >Search destinations</p>
-                    </section>
-                    <section className='btn-datepicker'>
-                        <span className='bold'>Check in</span>
-                        <p >Add dates</p>
-                    </section>
-                    <section className='btn-datepicker'>
-                        <span className='bold'>Check out</span>
-                        <p >Add dates</p>
-                    </section>
-                    <section className='btn-datepicker'>
-                        <span className='bold'>Who</span>
-                        <p className='block' >Add guests</p>
-                    </section>
-                    <IoSearch className='search-btn' />
-                </section>
+                </section>}
             </header>
-            }
         </>
     )
 }
