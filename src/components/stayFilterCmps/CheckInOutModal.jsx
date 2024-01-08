f3600189ec81ce911f7a98504ead393d0b4bae80
YardenheStudent <yardenhe@post.bgu.ac,il>
2024-01-08 17:05:51 +0200

    *.FEATURE-add add loc to filter

---
+++ src/components/stayFilterCmps/CheckInOutModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function CheckInOutModal() {
+    return (
+        <div className='location-modal'>CheckInOutModal</div>
+    )
+}
---
+++ src/components/stayFilterCmps/GuestsModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function GuestsModal() {
+    return (
+        <div className='location-modal'>GuestsModal</div>
+    )
+}
